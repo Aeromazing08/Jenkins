701fd80649aaa0f20a2573d49823fda80ecc2245
Aeromazing08 <55785090+Aeromazing08@users.noreply.github.com>
2019-10-02 11:14:18 +0800

    Update outputs.java

--- outputs.java
+++ outputs.java
@@ -2,7 +2,7 @@ public class outputs
 {
   public static void main(String[]args)
    {
-      String FName = "Ae", MI = "C", LName = "Robles"
+      String FName = "Ae", MI = "C", LName = "Robles";
       System.out.println("First Name : " + FName);
       System.out.println("First Name : " + MI);
       System.out.println("First Name : " + LName);
